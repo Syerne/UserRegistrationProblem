6692ae24e4159df67f32d19aba4fabc69194b83b
Syerne <shubhamyerne1994@gmail.com>
2022-07-17 19:10:48 +0530

    Last Name Validate

--- src/com/java/validate/UserRegistrationProblem.java
+++ src/com/java/validate/UserRegistrationProblem.java
@@ -4,9 +4,14 @@ import java.util.regex.Pattern;
 
 public class UserRegistrationProblem {
     private static final String FIRST_NAME_REGEX = "[A-Z][a-z]{2,}";
+    private static final String LAST_NAME_REGEX = "[A-Z][a-z]{2,}";
 
     public boolean isValidFirstName(String firstName) {
         Pattern pattern = Pattern.compile(FIRST_NAME_REGEX);
         return pattern.matcher(firstName).matches();
+    }
+    public boolean isValidLastName(String lastName){
+        boolean b = Pattern.matches(LAST_NAME_REGEX, lastName);
+        return b;
     }
 }
--- test/com/java/validate/TestUserRegistrationProblem.java
+++ test/com/java/validate/TestUserRegistrationProblem.java
@@ -1,15 +1,25 @@
 package com.java.validate;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestUserRegistrationProblem {
 
+    static UserRegistrationProblem valid;
+    @BeforeClass
+    public static void beforeClass(){
+        valid = new UserRegistrationProblem();
+    }
 
     @Test
     public void givenFirstName_whenProper_shouldReturnTrue() {
-        UserRegistrationProblem valid = new UserRegistrationProblem();
         boolean result = valid.isValidFirstName("Shubham");
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTrue(){
+        boolean result = valid.isValidLastName("Yerne");
         Assert.assertTrue(result);
     }
 }
